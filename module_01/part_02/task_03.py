cdffe2245cc1b453e4e26906f831ed4863b318c2
Артем <tyomainteme@yandex.ru>
2021-07-20 17:21:53 +0300

    module_01 fixes part2\tasks_03

--- module_01/part_02/task_03.py
+++ module_01/part_02/task_03.py
@@ -1,3 +1,14 @@
 password = input('Введите пароль: ')
-while len(password) < 8 or password.islower() or password.isupper():
+while True:
+	i = 0
+	count_1 = 0
+	count_2 = 0
+	while i < len(password):
+		if password[i].islower():
+			count_1 += 1
+		elif password[i].isupper():
+			count_2 += 1
+		i += 1
+	if len(password) > 8 and count_1 >= 1 and count_2 >= 1:
+		break
 	password = input('Повторите ввод пароля: ')
