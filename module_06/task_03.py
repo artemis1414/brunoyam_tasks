7f70703fd4aa21e01f6beb1b9168df668a4cda77
Artem Dobikov <soadob14@gmail.com>
2021-07-28 16:11:26 +0300

    fix task_03

--- module_06/task_03.py
+++ module_06/task_03.py
@@ -1,11 +1,13 @@
 import time
-from requests import get
+import requests
 from threading import Thread
 
 def get_html(link):
-    text_web = get(link)
+    response = requests.get(link)
+    head = response.headers
+    body = [head[i] for i in head]
     print(f'Загрузка {link} завершена успешно!')
-    return text_web
+    return body
 
 
 list_html = ['https://yandex.ru/', 'https://github.com/', 'https://www.google.ru/',
@@ -14,7 +16,7 @@ threads = [Thread(target=get_html, args=[list_html[i]]) for i in range(5)]
 
 t_seq = time.time()
 for i in range(5):
-    get_html(list_html[i])
+    print(get_html(list_html[i]))
 print(f'Время последовательного выполнения: {time.time() - t_seq}')
 t_par = time.time()
 for start in threads:
